d33c10ae2bcf0d6f8ba0ba48f62a1efe03278a07
x0446891 <fabriciosouza@santos.sp.gov.br>
2022-12-23 15:30:32 -0300

    add routes with controller

--- src/controllers/BlockedDatesController.js
+++ src/controllers/BlockedDatesController.js
@@ -1,7 +1,44 @@
-class BlockedDatesController {
-  async index(request, response) {
-    return response.json([]);
+const db = require('../database/connections');
+
+const blockedDatesController = {
+  async index(req, res) {
+    const models = await db('blockedDate').select('*');
+
+    return res.json(models);
+  },
+
+  async show(req, res) {
+    const { id } = req.params;
+
+    const [model] = await db('blockedDate').where('id', id);
+
+    return res.json(model);
+  },
+
+  async create(req, res) {
+    const { start, end } = req.body;
+
+    await db('blockedDate').insert([ { start, end } ]);
+
+    return res.status(201).send();
+  },
+
+  async update(req, res) {
+    const { id } = req.params;
+
+    await db('blockedDate').where('id', id)
+    .update({ ...req.body });
+
+    return res.status(204).send();
+  },
+
+  async remove(req, res) {
+    const { id } = req.params;
+
+    await db('blockedDate').where('id', id).del();
+
+    return res.status(204).send();
   }
 }
 
-module.exports = BlockedDatesController;
+module.exports = blockedDatesController;
--- src/routes.js
+++ src/routes.js
@@ -7,5 +7,9 @@ const routes = express.Router();
 const blockedDatesController = new BlockedDatesController();
 
 routes.get('/blocked-dates', blockedDatesController.index);
+routes.get('/blocked-dates/:id', blockedDatesController.show);
+routes.post('/blocked-dates', blockedDatesController.create);
+routes.put('/blocked-dates/:id', blockedDatesController.update);
+routes.delete('/blocked-dates/:id', blockedDatesController.remove);
 
 module.exports = routes;
